75715b24f92f3c6e9a3c1cb50494c1e71e8a972f
Viking <wiktor.przadka.1@gmail.com>
2021-12-13 12:02:34 +0100

    solution of day13

---
+++ AoE2021/day13/star2.py
@@ -0,0 +1,36 @@
+def print_grid(points: set):
+    max_x = max([x for x, _ in points])
+    max_y = max([y for _, y in points])
+    grid = [['.' for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+    for x, y in points:
+        grid[y][x] = '#'
+    for row in grid:
+        print(row)
+
+
+if __name__ == '__main__':
+
+    dots = set()
+    folds = []
+    with open('fst.txt') as f:
+        line = f.readline()
+        while line != '\n':
+            dots.add(tuple(int(v) for v in line.rstrip().split(',')))
+            line = f.readline()
+
+        line = f.readline()
+        while line:
+            a, b = line.split('=')
+            folds.append((a[-1], int(b.rstrip())))
+            line = f.readline()
+    print(dots)
+    print(folds)
+
+    for axis, val in folds:
+        if axis == 'x':
+            dots = {(2*val - x, y) if x > val else (x, y) for x, y in dots}
+        else:
+            dots = {(x, 2*val - y) if y > val else (x, y) for x, y in dots}
+
+    print(len(dots))
+    print_grid(dots)  # UCLZRAZU
